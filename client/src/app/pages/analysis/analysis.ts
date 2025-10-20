0cb0784baa56bf2a7122e8b8566161cfbe39f220
Heng Jake <hengjunkai@gmail.com>
2025-10-20 12:57:31 +0800

    init MEAN stack

---
+++ client/src/app/pages/analysis/analysis.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis',
+  imports: [],
+  template: `
+    <p>
+      analysis works!
+    </p>
+  `,
+  styles: ``
+})
+export class Analysis {
+
+}
---
+++ client/src/app/pages/canva/canva.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canva',
+  imports: [],
+  template: `
+    <p>
+      canva works!
+    </p>
+  `,
+  styles: ``
+})
+export class Canva {
+
+}
